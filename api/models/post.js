cd85526aee3d278ad8980affa0b97fce1b41daaa
HJain13 <himeshjain13@gmail.com>
2019-12-19 17:39:46 +0530

    Cleaned up api folder and added DB related code

---
+++ api/models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const post = new Schema(
+	{
+		content: { type: String, required: true },
+		upvotes: { type: Number, default: 0 }
+	},
+	{ timestamps: true }
+)
+
+module.exports = mongoose.model('posts', post)
