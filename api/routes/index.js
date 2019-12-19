94355a3972767f59247e46414f93c10c4f7804c6
HJain13 <himeshjain13@gmail.com>
2019-12-19 17:40:17 +0530

    feat: Added all the API endpoints and logic

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,50 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+const Post = require('../models/post')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/posts', (req, res) => {
+	Post.find({})
+		.sort({ createdAt: -1 })
+		.exec()
+		.then((posts) => res.status(200).json({ success: true, posts }))
+		.catch((error) => res.status(400).json({ success: false, error }))
+})
 
-module.exports = router;
+router.post('/post', function(req, res) {
+	const body = req.body
+	if (!body) {
+		return res.status(400).json({
+			success: false,
+			error: 'No Post Provided'
+		})
+	}
+
+	const post = new Post(body)
+	if (!post) {
+		return res.status(400).json({ success: false })
+	}
+	post.save()
+		.then(() => res.status(201).json({ success: true, post }))
+		.catch((error) => res.status(400).json({ success: false, error }))
+})
+
+router.put('/post/upvote/:id', (req, res) => {
+	const body = req.body
+	if (!body) {
+		return res.status(400).json({
+			success: false
+		})
+	}
+
+	Post.findOneAndUpdate({ _id: req.params.id }, { $inc: { upvotes: 1 } }, { new: true })
+		.exec()
+		.then((post) => res.status(200).json({ success: true, post }))
+		.catch((error) => {
+			console.log(error)
+			res.status(404).json({ success: false, error })
+		})
+})
+
+module.exports = router
